09b97ec5dba86e799652572581cf0352198dd4de
mahmoud <m.abdulaziz@bstt.com.sa>
2022-09-26 15:26:13 +0200

    [FIX] purchase_committee_approval

--- purchase_committee_approval/models/stock_move.py
+++ purchase_committee_approval/models/stock_move.py
@@ -15,15 +15,12 @@ class StockMove(models.Model):
     def create(self, vals):
         move = super(StockMove, self).create(vals)
         if 'purchase_line_id' in vals:
-            move.write({'product_uom_qty': move.purchase_line_id.accept_qty})
+            move.write({'quantity_done': move.purchase_line_id.accept_qty})
         return move
 
-    @api.constrains('accept_purchase_qty', 'quantity_done', 'purchase_line_id', 'state')
-    @api.onchange('accept_purchase_qty', 'quantity_done', 'purchase_line_id', 'state')
     def check_accept_purchase_qty(self):
         for rec in self:
             if rec.purchase_line_id and rec.purchase_line_id.order_id.require_committee_approval:
-                if rec.quantity_done > rec.accept_purchase_qty:
-                    raise ValidationError(
-                        "{} : Sorry you can not accept quantity more than {}.".format(rec.product_id.name,
-                                                                                      rec.accept_purchase_qty))
+                if rec.quantity_done != rec.accept_purchase_qty:
+                    raise ValidationError("{} : Sorry you must accept only {}.".format(rec.product_id.name,
+                                                                                       rec.accept_purchase_qty))
--- purchase_committee_approval/models/stock_picking.py
+++ purchase_committee_approval/models/stock_picking.py
@@ -8,8 +8,19 @@ from odoo.exceptions import ValidationError
 class StockMove(models.Model):
     _inherit = 'stock.picking'
 
+    is_purchase_order = fields.Boolean(compute='_compute_purchase_order')
+
+    @api.depends('move_ids_without_package')
+    def _compute_purchase_order(self):
+        for rec in self:
+            rec.is_purchase_order = True if rec.move_ids_without_package.mapped('purchase_line_id') else False
 
     def action_set_quantities_to_reservation(self):
         super(StockMove, self).action_set_quantities_to_reservation()
         for line in self.move_ids_without_package:
             line.check_accept_purchase_qty()
+
+    def button_validate(self):
+        for line in self.move_ids_without_package:
+            line.check_accept_purchase_qty()
+        return super(StockMove, self).button_validate()
