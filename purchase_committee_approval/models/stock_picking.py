c149f9015bcce31c7b13ae63346f4d08c10b4326
mahmoud <m.abdulaziz@bstt.com.sa>
2022-09-28 18:48:26 +0200

    [UPD] purchase_committee_approval: backorder approval

--- purchase_committee_approval/models/stock_picking.py
+++ purchase_committee_approval/models/stock_picking.py
@@ -5,22 +5,38 @@ from odoo.exceptions import Warning
 from odoo.exceptions import ValidationError
 
 
-class StockMove(models.Model):
+class StockPicking(models.Model):
     _inherit = 'stock.picking'
 
-    is_purchase_order = fields.Boolean(compute='_compute_purchase_order')
+    purchase_order_id = fields.Many2one('purchase.order', compute='_compute_purchase_order')
+    active = fields.Boolean('Active', default=True, tracking=True)
 
     @api.depends('move_ids_without_package')
     def _compute_purchase_order(self):
         for rec in self:
-            rec.is_purchase_order = True if rec.move_ids_without_package.mapped('purchase_line_id') else False
+            purchase_order_lines = rec.move_ids_without_package.mapped('purchase_line_id')
+            if purchase_order_lines:
+                rec.purchase_order_id = purchase_order_lines[0].order_id
+            else:
+                rec.purchase_order_id = False
 
     def action_set_quantities_to_reservation(self):
-        super(StockMove, self).action_set_quantities_to_reservation()
+        super(StockPicking, self).action_set_quantities_to_reservation()
         for line in self.move_ids_without_package:
             line.check_accept_purchase_qty()
 
     def button_validate(self):
         for line in self.move_ids_without_package:
             line.check_accept_purchase_qty()
-        return super(StockMove, self).button_validate()
+        if self.purchase_order_id and self.purchase_order_id.require_committee_approval:
+            return super(StockPicking, self.with_context(skip_backorder=True)).button_validate()
+        else:
+            return super(StockPicking, self).button_validate()
+
+    @api.model
+    def create(self, vals):
+        picking = super(StockPicking, self).create(vals)
+        origin = picking.backorder_id
+        if origin and origin.purchase_order_id and origin.purchase_order_id.require_committee_approval:
+            picking.active = False
+        return picking
--- purchase_committee_approval/wizard/backorder_commit_approval.py
+++ purchase_committee_approval/wizard/backorder_commit_approval.py
@@ -3,43 +3,34 @@ from odoo import api, fields, models
 
 
 class purchaseCommitteeapproval(models.Model):
-    _name = 'purchase.committee.approval'
-    _description = 'Purchase Committee approval'
+    _name = 'backorder.committee.approval'
+    _description = 'backorder Committee approval'
 
     name = fields.Char()
     purchase_id = fields.Many2one('purchase.order', 'Purchase')
     committee_decision = fields.Selection(selection=[('approve', 'Approve'), ('refuse', 'Refuse')])
 
-    line_ids = fields.One2many('purchase.committee.approval.line', 'approval_id', "Approval Lines")
-    quantity_line_ids = fields.One2many('purchase.quantity.approval.line', 'approval_id', "Quantity Lines")
-    show_backorder = fields.Boolean(compute='_compute_show_backorder', default=False)
-
-    @api.depends('quantity_line_ids', 'quantity_line_ids.qty_received')
-    def _compute_show_backorder(self):
-        for rec in self:
-            rec.show_backorder = False
-            for line in rec.quantity_line_ids.filtered(lambda p: p.qty_received > 0):
-                if line.qty_received < line.product_qty:
-                    rec.show_backorder = True
-                    return
+    line_ids = fields.One2many('backorder.committee.approval.line', 'approval_id', "Approval Lines")
+    quantity_line_ids = fields.One2many('backorder.quantity.approval.line', 'approval_id', "Quantity Lines")
 
     def action_approve(self):
-        if self.purchase_id.state == 'purchase':
-            self.purchase_id.state = 'draft'
-            self.purchase_id.button_confirm()
-        self.committee_decision = 'approve'
+        backorder = self.env['stock.picking'].search(
+            [('purchase_order_id', '=', self.purchase_id), ('active', '=', False)])
+        for move in backorder.move_lines:
+            move.write({
+                'quantity_done': move.purchase_line_id.backorder_accept_qty,
+                'accept_purchase_qty': move.purchase_line_id.backorder_accept_qty
+            })
+        backorder.active = True
 
     def action_refuse(self):
         self.committee_decision = 'refuse'
-
-    def create_backorder(self):
-        self.purchase_id.require_backorder_committee = True
 
     def check_quantity_approval(self):
         if self.purchase_id:
             for line in self.purchase_id.order_line:
                 if not self.quantity_line_ids.filtered(lambda l: l.order_line_id == line):
-                    self.env['purchase.quantity.approval.line'].create({
+                    self.env['backorder.quantity.approval.line'].create({
                         'order_line_id': line.id,
                         'approval_id': self.id
                     })
@@ -49,18 +40,18 @@ class purchaseCommitteeapproval(models.Model):
             employees = self.purchase_id.requisition_id.purchase_committee_member_ids
             for employee in employees:
                 if not self.line_ids.filtered(lambda l: l.employee_id == employee):
-                    self.env['purchase.committee.approval.line'].create({
+                    self.env['backorder.committee.approval.line'].create({
                         'employee_id': employee.id,
                         'approval_id': self.id
                     })
 
 
 class purchaseCommitteeapprovalLine(models.Model):
-    _name = 'purchase.committee.approval.line'
-    _description = 'Purchase Committee approval Line'
+    _name = 'backorder.committee.approval.line'
+    _description = 'backorder Committee approval Line'
 
     name = fields.Char()
-    approval_id = fields.Many2one('purchase.committee.approval')
+    approval_id = fields.Many2one('backorder.committee.approval')
     user_id = fields.Many2one('res.users', 'المستخدم', readonly=1)
     employee_id = fields.Many2one('hr.employee', 'الموظف', readonly=1)
     accept = fields.Boolean('موافقه')
@@ -74,14 +65,22 @@ class purchaseCommitteeapprovalLine(models.Model):
 
 
 class purchaseQuantityapprovalLine(models.Model):
-    _name = 'purchase.quantity.approval.line'
+    _name = 'backorder.quantity.approval.line'
     _description = 'Purchase Quantity approval Line'
 
-    approval_id = fields.Many2one('purchase.committee.approval')
+    approval_id = fields.Many2one('backorder.committee.approval')
     order_line_id = fields.Many2one('purchase.order.line')
     product_id = fields.Many2one('product.product', related='order_line_id.product_id', string='الصنف', readonly=True)
     product_qty = fields.Float(string='الكمية المطلوبة', related='order_line_id.product_qty', readonly=True)
     qty_received = fields.Float(string='الكمية المستلمة', related='order_line_id.qty_received', readonly=True)
+    qty_remainder = fields.Float(string='الكمية المتبقية', compute='_compute_qty_remainder', readonly=True)
     price_unit = fields.Float(string='سعر الوحدة', related='order_line_id.price_unit', readonly=True)
     accept = fields.Boolean(related='order_line_id.accept', string='موافقه', readonly=True)
     accept_qty = fields.Float(string='الكمية المقبولة', related='order_line_id.accept_qty', readonly=False)
+    backorder_accept_qty = fields.Float(string='الكمية المقبولة', related='order_line_id.backorder_accept_qty',
+                                        readonly=False)
+
+    @api.depends('product_qty', 'qty_received')
+    def _compute_qty_remainder(self):
+        for rec in self:
+            rec.qty_remainder = rec.product_qty - rec.qty_received
