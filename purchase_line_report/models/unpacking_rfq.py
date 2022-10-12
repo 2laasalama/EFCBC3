5d6049cf29f2543437a8ac39d017f79328131bbb
Mabdelaziz <mahmoud.abdalaziz20@gmail.com>
2022-10-12 11:55:26 +0200

    [IMP] purchase_line_report: update report

--- purchase_line_report/models/unpacking_rfq.py
+++ purchase_line_report/models/unpacking_rfq.py
@@ -10,6 +10,7 @@ class UnpackingRFQ(models.Model):
 
     requisition_id = fields.Many2one('purchase.requisition')
     line_ids = fields.One2many('unpacking.rfq.line', 'unpacking_id')
+    edit_date = fields.Date('تاريخ التحرير')
 
     def get_partners(self):
         partners = []
@@ -28,14 +29,17 @@ class UnpackingRFQ(models.Model):
     def get_amount_totals(self, accept=False):
         totals = []
         for partner in self.get_partners():
-            accept_lines = self.line_ids.filtered(lambda x: x.partner_id.id == partner['id'] and x.accept)
+            accept_lines = self.line_ids.filtered(
+                lambda x: x.partner_id.id == partner['id'] and x.accept)
             if accept:
-                line_ids = self.line_ids.filtered(lambda x: x.partner_id.id == partner['id'] and x.accept)
+                line_ids = self.line_ids.filtered(
+                    lambda x: x.partner_id.id == partner['id'] and x.accept)
             else:
                 line_ids = self.line_ids.filtered(lambda x: x.partner_id.id == partner['id'])
             amount_total = sum(x.price_total for x in line_ids)
             if accept_lines:
-                amount_total_txt = self.requisition_id.currency_id.with_context(lang='ar_001').amount_to_text(
+                amount_total_txt = self.requisition_id.currency_id.with_context(
+                    lang='ar_001').amount_to_text(
                     amount_total)
                 totals.append({
                     'sequence': partner['sequence'],
@@ -50,7 +54,8 @@ class UnpackingRFQ(models.Model):
 
         for order in self.requisition_id.purchase_ids.sorted('amount_total'):
             sequence = self.get_partner_sequence(order.partner_id.id)
-            amount_total_txt = order.currency_id.with_context(lang='ar_001').amount_to_text(order.amount_total)
+            amount_total_txt = order.currency_id.with_context(lang='ar_001').amount_to_text(
+                order.amount_total)
             orders.append({
                 'order_id': order,
                 'sequence': sequence,
@@ -74,7 +79,8 @@ class UnpackingRFQ(models.Model):
             for line in order_lines.filtered(lambda x: x.product_id.id == product.id):
                 add = True
                 for item in res:
-                    if item['product_id'] == product.id and item['partner_id'] == line.order_id.partner_id.id:
+                    if item['product_id'] == product.id and item[
+                        'partner_id'] == line.order_id.partner_id.id:
                         add = False
                 if add:
                     vals.update({
@@ -88,7 +94,8 @@ class UnpackingRFQ(models.Model):
         return res
 
     def action_print(self):
-        return self.env.ref('purchase_line_report.purchase_line_xlsx').report_action(self.line_ids.ids)
+        return self.env.ref('purchase_line_report.purchase_line_xlsx').report_action(
+            self.line_ids.ids)
 
     def print_unpacking_report(self):
         return self.env.ref('purchase_line_report.unpacking_report').report_action(self.id)
@@ -103,7 +110,8 @@ class UnpackingRFQ(models.Model):
         for rec in self:
             accept_lines = rec.line_ids.filtered(lambda l: l.accept)
             for line in accept_lines:
-                if line.accept and accept_lines.filtered(lambda l: l.product_id == line.product_id and l.id != line.id):
+                if line.accept and accept_lines.filtered(
+                        lambda l: l.product_id == line.product_id and l.id != line.id):
                     raise ValidationError("You Can't Accept on product from more than one vendor.")
 
 
@@ -112,14 +120,20 @@ class UnpackingRFQLine(models.Model):
 
     unpacking_id = fields.Many2one('unpacking.rfq')
     order_line_id = fields.Many2one('purchase.order.line')
-    partner_id = fields.Many2one('res.partner', related='order_line_id.partner_id', string='المورد', readonly=True)
-    product_id = fields.Many2one('product.product', related='order_line_id.product_id', string='الصنف', readonly=True)
+    partner_id = fields.Many2one('res.partner', related='order_line_id.partner_id', string='المورد',
+                                 readonly=True)
+    product_id = fields.Many2one('product.product', related='order_line_id.product_id',
+                                 string='الصنف', readonly=True)
     product_name = fields.Char(string='الصنف', readonly=True)
-    price_unit = fields.Float(string='سعر الوحدة', related='order_line_id.price_unit', readonly=True)
+    price_unit = fields.Float(string='سعر الوحدة', related='order_line_id.price_unit',
+                              readonly=True)
     product_qty = fields.Float(string='الكمية', related='order_line_id.product_qty', readonly=True)
-    price_subtotal = fields.Monetary(related='order_line_id.price_subtotal', string='الاجمالى', readonly=True)
-    price_total = fields.Monetary(related='order_line_id.price_total', string=' الاجمالى شامل الضريبة', readonly=True)
-    date_order = fields.Datetime(related='order_line_id.date_order', string=' تاريخ الاستلام', readonly=True)
+    price_subtotal = fields.Monetary(related='order_line_id.price_subtotal', string='الاجمالى',
+                                     readonly=True)
+    price_total = fields.Monetary(related='order_line_id.price_total',
+                                  string=' الاجمالى شامل الضريبة', readonly=True)
+    date_order = fields.Datetime(related='order_line_id.date_order', string=' تاريخ الاستلام',
+                                 readonly=True)
     accept = fields.Boolean(related='order_line_id.accept', string='موافقه', readonly=False, )
     currency_id = fields.Many2one('res.currency', 'Currency', required=True,
                                   default=lambda self: self.env.company.currency_id.id)
