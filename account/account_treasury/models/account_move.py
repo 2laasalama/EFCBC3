0fdb434928c924625ec23be5f8159a47626d04b2
Mabdelaziz <mahmoud.abdalaziz20@gmail.com>
2023-01-09 15:30:04 +0200

    [FIX] account_treasury: default value

--- account/account_treasury/models/account_move.py
+++ account/account_treasury/models/account_move.py
@@ -2,20 +2,15 @@ from odoo import models, api, fields, _
 from odoo.exceptions import ValidationError
 
 
-class AccountPayment(models.Model):
-    _inherit = "account.payment"
+class AccountMove(models.Model):
+    _inherit = "account.move"
 
     def get_treasury_journal_id(self):
         journal_id = self.env['account.journal'].search([('is_expense_treasury', '=', True)], limit=1)
         return journal_id.id if journal_id else False
 
     @api.model
-    def _get_default_journal(self):
+    def _search_default_journal(self, journal_types):
         if self._context.get('default_is_treasury_promotion'):
             return self.get_treasury_journal_id()
-        return super()._get_default_journal()
-
-    is_treasury_promotion = fields.Boolean()
-    treasury_code = fields.Char(related='journal_id.code')
-
-
+        return super()._search_default_journal(journal_types)
